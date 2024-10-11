7452cddc78c64d970b69400c99b33c4af7eaa41d
Hassan Khan <h@twodoors.dev>
2024-10-11 15:09:37 +0100

    chore: initial commit

---
+++ apps/mobile/src/__tests__/App.spec.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from '@testing-library/react-native';
+
+import Index from '../app';
+
+test('renders correctly', () => {
+  const { getByTestId } = render(<Index />);
+  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+});
