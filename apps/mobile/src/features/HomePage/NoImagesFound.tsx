0443012472bf9fa1c90fb140e54226c6b03832c6
Hassan Khan <h@twodoors.dev>
2024-10-12 23:08:26 +0100

    fix(apps/mobile): improve no images found component

--- apps/mobile/src/features/HomePage/NoImagesFound.tsx
+++ apps/mobile/src/features/HomePage/NoImagesFound.tsx
@@ -1,3 +1,4 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Text, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
@@ -5,17 +6,28 @@ export const NoImagesFound = () => {
   const { styles } = useStyles(stylesheet);
   return (
     <View style={styles.root}>
-      <View>
+      <View style={styles.message}>
+        <FontAwesome name="warning" size={24} color="grey" />
         <Text>No images found</Text>
       </View>
-      <Text>Imagine a vertical arrow pointing towards the + button</Text>
+      {/*<Text>Imagine a vertical arrow pointing towards the + button</Text>*/}
     </View>
   );
 };
 
 const stylesheet = createStyleSheet((theme) => ({
   root: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  message: {
+    borderRadius: 15,
+    padding: 16,
+    backgroundColor: 'lightgrey',
+    justifyContent: 'center',
+    alignItems: 'center',
+    columnGap: 16,
+    flexDirection: 'row',
   },
 }));
