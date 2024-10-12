2aa0efc7114cd3eaac6c43e25da5a4871ce51583
Hassan Khan <h@twodoors.dev>
2024-10-12 06:45:26 +0100

    feat(apps/mobile): make center button more ... interesting

--- apps/mobile/src/components/UploadButton.tsx
+++ apps/mobile/src/components/UploadButton.tsx
@@ -1,7 +1,7 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { router } from 'expo-router';
 import { useCallback } from 'react';
-import { Pressable } from 'react-native';
+import { Pressable, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 export const UploadButton = () => {
@@ -13,19 +13,54 @@ export const UploadButton = () => {
 
   return (
     <Pressable style={styles.root} onPress={handlePress}>
-      <FontAwesome size={28} name="plus-circle" color="blue" />
+      <View style={styles.leftEar}></View>
+      <View style={styles.rightEar}></View>
+      <View style={styles.button}>
+        <FontAwesome size={28} name="camera" color="white" />
+      </View>
     </Pressable>
   );
 };
 
 const stylesheet = createStyleSheet({
   root: {
-    height: 90,
-    width: 90,
-    backgroundColor: 'white',
-    borderRadius: 100,
-    marginVertical: -45,
+    // height: 90,
+    // width: 90,
+    // backgroundColor: 'white',
+    // borderRadius: 100,
+    // marginVertical: -45,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  leftEar: {
+    position: 'absolute',
+    top: -40,
+    left: 0,
+    height: 55,
+    width: 20,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    backgroundColor: 'black',
+  },
+  rightEar: {
+    position: 'absolute',
+    top: -40,
+    right: 0,
+    height: 55,
+    width: 20,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 15,
+    borderBottomRightRadius: 25,
+    backgroundColor: 'black',
+  },
+  button: {
+    height: 60,
+    width: 60,
+    marginVertical: -30,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black',
+    borderRadius: 100,
   },
 });
