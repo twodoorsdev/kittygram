c1685829b19324d3b97c8ff96ea6b347ee4e21b8
Hassan Khan <h@twodoors.dev>
2024-10-12 03:16:50 +0100

    refactor(apps/mobile): simplify logic in photo library retrieval thunk

--- apps/mobile/src/store/thunks/getPhotoLibrary.ts
+++ apps/mobile/src/store/thunks/getPhotoLibrary.ts
@@ -5,13 +5,11 @@ import { createAppAsyncThunk } from '../overrides';
 export const getPhotoLibrary = createAppAsyncThunk(
   'photos/getLibrary',
   async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
+    return ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       // allowsEditing: true,
       // aspect: [4, 3],/
       // quality: 1,
     });
-
-    return result;
   }
 );
